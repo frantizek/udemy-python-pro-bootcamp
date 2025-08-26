2307eb010e1272b3f4fdb0b8aee2f80f79ccba27
Francisco Ruvalcaba C <32653773+frantizek@users.noreply.github.com>
2025-08-26 16:45:20 -0600

    day 001 task completed

--- src/day_001.py
+++ src/day_001.py
@@ -1,1 +1,4 @@
 print("Welcome to the Band Name Generator.")
+city_name = str(input("What's the name of the city you grew up? : "))
+pet_name = str(input("What's your pet's name? : "))
+print(f"Your band name could be {city_name.capitalize()} {pet_name.capitalize()}")
