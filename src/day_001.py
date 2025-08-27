ced7ef7387bfe117316a2879b153c42b51a15497
Francisco Ruvalcaba C <32653773+frantizek@users.noreply.github.com>
2025-08-26 18:40:55 -0600

    day 001 task completed with more structure

--- src/day_001.py
+++ src/day_001.py
@@ -1,4 +1,9 @@
-print("Welcome to the Band Name Generator.")
-city_name = str(input("What's the name of the city you grew up? : "))
-pet_name = str(input("What's your pet's name? : "))
-print(f"Your band name could be {city_name.capitalize()} {pet_name.capitalize()}")
+def main() -> None:
+    print("Welcome to the Band Name Generator.")
+    city_name = str(input("What's the name of the city you grew up? : "))
+    pet_name = str(input("What's your pet's name? : "))
+    print(f"Your band name could be {city_name.capitalize()} {pet_name.capitalize()}")
+
+
+if __name__ == "__main__":
+    main()
